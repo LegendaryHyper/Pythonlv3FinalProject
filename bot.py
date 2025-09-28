560422a6f39a697b6f345530aae29fd0706d2014
İbrahim Deniz Altun <lh999space@gmail.com>
2025-09-28 14:12:43 +0300

    Added uses

--- bot.py
+++ bot.py
@@ -31,10 +31,17 @@ async def work(ctx):
     manager.set_balance(user_id, income)
     await ctx.send(f"Woah, {ctx.author.mention}! For your efforts, you just got {income} bucks!")
 @bot.command()
-async def add_item(ctx, item_name, item_cost):
+async def add_item(ctx, item_name, item_cost, sold, use):
     if ctx.author.id == 722496876351455293:
-        manager.add_to_shop(item_name, item_cost)
-        await ctx.send(f"Successfully added {item_name} to the shop for {item_cost}!")
+        manager.add_item(item_name, item_cost, int(sold), int(use))
+        await ctx.send(f"Successfully added {item_name} to the shop for {item_cost}\n Item for sale: {bool(int(sold))}\nUsage Index: {int(use)}!")
+    else:
+        await ctx.send("Sorry, no perms!")
+@bot.command()
+async def add_use(ctx, use_name):
+    if ctx.author.id == 722496876351455293:
+        manager.add_use(use_name)
+        await ctx.send(f"Successfully added {use_name} as a way of usage!")
     else:
         await ctx.send("Sorry, no perms!")
 @bot.command()
@@ -48,6 +55,16 @@ async def shop(ctx):
     embed = discord.Embed(title = "Shop", description = output, color = 0xffff00)
     await ctx.send(embed = embed)
 @bot.command()
+async def uses(ctx):
+    output = "Use ID - Use Name\n"
+    uses_output = manager.get_uses()
+    if uses_output:
+        for i in uses_output: output += f"{i[0]} - {i[1]}\n"
+    else:
+        output += "No uses"
+    embed = discord.Embed(title = "All Item Uses", description = output, color = 0x009fff)
+    await ctx.send(embed = embed)
+@bot.command()
 async def buy(ctx, item_name, count):
     user_id = ctx.author.id
     delta = manager.get_cost(item_name)
@@ -59,8 +76,21 @@ async def buy(ctx, item_name, count):
         manager.set_balance(user_id, -delta*count_int)
         manager.update_inv(user_id, item_name, count_int)
         if count_int == 1:
-            await ctx.send(f"{ctx.author.mention} has successfully bought a(n) {item_name}!")
+            await ctx.send(f"{ctx.author.mention} has successfully bought a(n) {item_name} for {delta} bucks!")
         else:
-            await ctx.send(f"{ctx.author.mention} has successfully bought {count_int} {item_name}s!")
-    
+            await ctx.send(f"{ctx.author.mention} has successfully bought {count_int} {item_name}s for {delta*count_int} bucks!")
+@bot.command()
+async def use(ctx, item_name):
+    user_id = ctx.author.id # TO BE ADDED
+@bot.command()
+async def get_inv(ctx):
+    user_id = ctx.author.id
+    output = "Item Name - Count/Amount\n"
+    user_inv = manager.get_inv(user_id)
+    if user_inv:
+        output += user_inv
+    else:
+        output += "Nothing in the inventory"
+    embed = discord.Embed(title = f"Inventory of {ctx.author}", description = output, color = 0xffff00)
+    await ctx.send(embed = embed)
 bot.run(TOKEN)
--- logic.py
+++ logic.py
@@ -7,6 +7,8 @@ import re
 class DatabaseManager:
     def __init__(self, database):
         self.database = database
+        self.shop_counter = self.get_shop_counter()
+        self.use_counter = self.get_use_counter()
     def create_tables(self):
         conn = sqlite3.connect(self.database)
         with conn:
@@ -23,6 +25,16 @@ class DatabaseManager:
                                 item_id INTEGER PRIMARY KEY,
                                 item_name TEXT,
                                 cost INTEGER)
+                                ''')
+            conn.execute('''CREATE TABLE IF NOT EXISTS items(
+                                item_id INTEGER PRIMARY KEY,
+                                item_name TEXT,
+                                use INTEGER,
+                                FOREIGN KEY(use) REFERENCES uses(use))
+                                ''')
+            conn.execute('''CREATE TABLE IF NOT EXISTS uses(
+                                use_id INTEGER PRIMARY KEY,
+                                use TEXT)
                                 ''')
             conn.commit()
     def add_user(self, user_id, user_name):
@@ -45,23 +57,71 @@ class DatabaseManager:
             balance = cur.fetchall()[0]
             conn.execute("UPDATE balance_and_items SET balance = ? WHERE user_id = ?", (balance[0] + delta, user_id))
             conn.commit()
-    def add_to_shop(self, item_name, item_cost):
+    def add_item(self, item_name, item_cost, sold, use):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
             cur.execute("SELECT COUNT(*) FROM shop")
             item_id = cur.fetchall()[0][0] + 1
-            conn.execute("INSERT INTO shop (item_id, item_name, cost) VALUES(?, ?, ?)", (item_id, item_name, item_cost))
+            self.shop_counter = item_id
+            if sold:
+                conn.execute("INSERT INTO shop (item_id, item_name, cost) VALUES(?, ?, ?)", (item_id, item_name, item_cost))
+            conn.execute("INSERT INTO items (item_id, item_name, use) VALUES(?, ?, ?)", (item_id, item_name, use))
             no_space = item_name.replace(" ", "")
             conn.execute(f"ALTER TABLE balance_and_items ADD {no_space} 'INTEGER'")
             conn.execute(f"UPDATE balance_and_items SET {no_space} = 0")
             conn.commit()
+    def add_use(self, use_name):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT COUNT(*) FROM uses")
+            use_id = cur.fetchall()[0][0]
+            self.use_counter = use_id
+            conn.execute("INSERT INTO uses (use_id, use) VALUES(?, ?)", (use_id, use_name))
+            conn.commit()
+    def get_shop_counter(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT COUNT(*) FROM shop")
+            return cur.fetchall()[0][0]
+    def get_use_counter(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT COUNT(*) FROM uses")
+            return cur.fetchall()[0][0]
     def get_shop(self):
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
             cur.execute("SELECT item_name, cost FROM shop")
             return cur.fetchall()
+    def get_uses(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT use_id, use FROM uses")
+            return cur.fetchall()
+    def get_inv(self, user_id):
+        conn = sqlite3.connect(self.database)
+        skipper = 0
+        inventory = ""
+        with conn:
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM balance_and_items WHERE user_id = ?", (user_id,))
+            inv_list = cur.fetchall()[0]
+            for i in inv_list:
+                if skipper <= 1:
+                    skipper += 1
+                else:
+                    cur.execute("SELECT item_name FROM items WHERE item_id = ?", (skipper - 1,))
+                    item = cur.fetchall()[0][0]
+                    if i != 0:
+                        inventory += f"{item} - {i}\n"
+                    skipper += 1
+            return inventory
     def update_inv(self, user_id, item_name, delta):
         conn = sqlite3.connect(self.database)
         no_space = item_name.replace(" ", "")
